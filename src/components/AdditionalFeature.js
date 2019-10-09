4bd19e1d471e8892708b3e5d295e7370c06fb590
jambis <imjaamees@gmail.com>
2019-10-09 15:10:03 -0500

    MVP

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+
+import { buyItem } from "../actions";
 
 const AdditionalFeature = props => {
+  const handleClick = () => {
+    props.buyItem(props.feature);
+  };
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={handleClick}>
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(
+  null,
+  { buyItem }
+)(AdditionalFeature);
